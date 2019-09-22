3e1e4413078d2ced93e588f768b72cec22fe2422
ferchuqui <luisfernandofarji@hotmail.com>
2019-09-22 13:41:41 -0300

    Added Event listener to form

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ function initMap(){
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
+
+  const form = document.querySelector('.form');
+  const description = document.querySelector('input[name="description"]');
+  const address = document.querySelector('input[name="address"]');
+  const number = document.querySelector('input[name="number"]');
+  const coords = document.querySelector('input[name="coords"]');
+  const category = document.querySelector('input[name="category"]');
+
+  form.addEventListener('submit', function (e){
+    e.preventDefault()
+    
+    console.log ({
+      description: description.value,
+      address: address.value,
+      number: number.value,
+      coords: coords.value,
+      category: category.value
+    })
+  })
 }
 
 
